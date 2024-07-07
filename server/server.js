b0d50f74600fc23d2110eb0072052f2e3cdfa62d
Moutaz Ali <moutaz.ali.dev@gmail.com>
2024-07-07 14:28:17 +0300

    initilize apollo server with express, create typeDefs and resolvers

--- server/server.js
+++ server/server.js
@@ -1,14 +1,25 @@
-import cors from 'cors';
-import express from 'express';
-import { authMiddleware, handleLogin } from './auth.js';
+import cors from "cors";
+import express from "express";
+import { authMiddleware, handleLogin } from "./auth.js";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware as apolloMiddleware } from "@apollo/server/express4";
+import fs from "node:fs/promises";
+import { resolvers } from "./graphql/resolvers.js";
 
 const PORT = 9000;
 
 const app = express();
 app.use(cors(), express.json(), authMiddleware);
 
-app.post('/login', handleLogin);
+app.post("/login", handleLogin);
+
+const typeDefs = await fs.readFile("./graphql/schema.graphql", "utf-8");
+
+const server = new ApolloServer({ typeDefs, resolvers });
+await server.start();
+app.use("/graphql", apolloMiddleware(server));
 
 app.listen({ port: PORT }, () => {
   console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}/graphql`);
 });
