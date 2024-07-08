d5ce329070e4b2674d8568222b8e8842ab471515
Moutaz Ali <moutaz.ali.dev@gmail.com>
2024-07-08 12:33:02 +0300

    get all jobs in frontend using graphql-request package and display the real data on home page

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,12 +1,19 @@
-import JobList from '../components/JobList';
-import { jobs } from '../lib/fake-data';
+import { useEffect, useState } from "react";
+import JobList from "../components/JobList";
+import { getAllJobs } from "../lib/graphql";
 
 function HomePage() {
+  const [jobs, setJobs] = useState([]);
+  useEffect(() => {
+    const setAllJobs = async () => {
+      const allJobs = await getAllJobs();
+      setJobs(allJobs);
+    };
+    setAllJobs();
+  }, []);
   return (
     <div>
-      <h1 className="title">
-        Job Board
-      </h1>
+      <h1 className="title">Job Board</h1>
       <JobList jobs={jobs} />
     </div>
   );
