949a9fb0139a650f7a98545f94f2681231e58ef5
Moutaz Ali <moutaz.ali.dev@gmail.com>
2024-07-09 18:02:58 +0300

    implement proper error handling in resolvers when incorrect id for company or job

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -1,11 +1,24 @@
 import { getCompany } from "../db/companies.js";
 import { getCompanyJobs, getJob, getJobs } from "../db/jobs.js";
+import { GraphQLError } from "graphql";
 
 export const resolvers = {
   Query: {
     jobs: () => getJobs(),
-    job: (_, args) => getJob(args.id),
-    company: (_, args) => getCompany(args.id),
+    job: async (_, args) => {
+      const job = await getJob(args.id);
+      if (!job) {
+        throw customError(`can't find job with id : ${args.id}`);
+      }
+      return job;
+    },
+    company: async (_, args) => {
+      const company = await getCompany(args.id);
+      if (!company) {
+        throw customError(`can't find company with id : ${args.id}`);
+      }
+      return company;
+    },
   },
   Job: {
     date: (job) => {
@@ -16,4 +29,8 @@ export const resolvers = {
   Company: {
     jobs: (company) => getCompanyJobs(company.id),
   },
+};
+
+const customError = (message) => {
+  return new GraphQLError(message, { extensions: { code: "NOT_FOUND" } });
 };
