72b3f3cd5cf61b0c2081cf1c507e720bd3b56e54
Moutaz Ali <moutaz.ali.dev@gmail.com>
2024-07-10 22:51:55 +0300

    add authorization to QraphqlClient to send tokens if user is authenticated

--- client/src/lib/graphql.js
+++ client/src/lib/graphql.js
@@ -1,6 +1,16 @@
 import { GraphQLClient, gql } from "graphql-request";
+import { getAccessToken } from "./auth";
 
-const client = new GraphQLClient("http://localhost:9000/graphql");
+const client = new GraphQLClient("http://localhost:9000/graphql", {
+  headers: () => {
+    const accessToken = getAccessToken();
+    if (accessToken) {
+      return { Authorization: `Bearer ${accessToken}` };
+    } else {
+      return {};
+    }
+  },
+});
 
 export const getAllJobs = async () => {
   const query = gql`
